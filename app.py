20797222a526c1e1ec4ab4fe675b56cb20cd6f24
rameshucs06 <rameshdata63@gmail.com>
2025-01-21 15:24:00 +0530

    Prediction Pipeline

--- app.py
+++ app.py
@@ -32,14 +32,14 @@ def predict_datapoint():
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
-        print("Before Prediction")
+        #print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
+        #print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
+        #print("after Prediction")
         return render_template('home.html',results=results[0])
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0", debug=True)        
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
-
+import os
 
 class PredictPipeline:
     def __init__(self):
@@ -14,7 +14,7 @@ class PredictPipeline:
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            preprocessor = load_object(file_path=preprocessor_path)
             print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
